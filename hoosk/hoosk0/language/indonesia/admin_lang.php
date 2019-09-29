40cc1aa22684524b7ac419850a907a5170f36feb
Muhammad Irfan Luthfi <iir_irfan02@hotmail.com>
2019-09-29 09:02:45 +0700

    Indonesian language support for Hoosk

---
+++ hoosk/hoosk0/language/indonesia/admin_lang.php
@@ -0,0 +1,275 @@
+<?php
+//Navigation Bar
+$lang['nav_dash']			= "Dasbor";
+$lang['nav_pages']			= "Halaman";
+$lang['nav_pages_new']                  = "Halaman Baru";
+$lang['nav_pages_all']                  = "Semua Halaman";
+$lang['nav_posts']			= "Postingan";
+$lang['nav_posts_new']                  = "Postingan Baru";
+$lang['nav_posts_all']                  = "Seluruh Postingan";
+$lang['nav_categories_new']             = "Kategori Baru";
+$lang['nav_categories_all']             = "Semua Kategori";
+$lang['nav_users']			= "Pengguna";
+$lang['nav_users_new']                  = "Pengguna Baru";
+$lang['nav_users_all']                  = "Semua Pengguna";
+$lang['nav_navigation']                 = "Navigasi";
+$lang['nav_navigation_new']             = "Menu Navigasi Baru";
+$lang['nav_navigation_all']             = "Seluruh Menu";
+$lang['nav_settings']                   = "Pengaturan";
+$lang['nav_social']			= "Media Sosial";
+$lang['nav_view_site']                  = "Lihat Situs";
+$lang['nav_logout']			= "Keluar";
+$lang['nav_profile']                    = "Profil";
+
+
+//Login Page
+$lang['login_message']                  = "Mohon masukkan detail akun Anda";
+$lang['login_username']                 = "Username";
+$lang['login_password']                 = "Password";
+$lang['login_signin']                   = "Masuk";
+$lang['login_reset']                    = "Set Ulang Password";
+$lang['login_incorrect']                = "Username dan Password yang dimasukkan salah";
+$lang['login_expired']                  = "Sesi Anda telah berakhir, gunakan form di bawah ini untuk kembali masuk ke dalam sistem";
+
+//Forgot Password
+$lang['forgot_reset']                   = "Set Ulang Password";
+$lang['forgot_email']                   = "Alamat Email";
+$lang['forgot_password']                = "Password Baru";
+$lang['forgot_confirm']                 = "Konfirmasi Password";
+$lang['forgot_complete']                = "Password Anda sudah diset ulang";
+$lang['forgot_btn']			= "Set Ulang";
+$lang['forgot_check_email']             = "Mohon periksa email Anda untuk mengetahui instruksi lebih lanjut";
+
+
+
+//Dashboard
+$lang['dash_welcome']					= "Selamat Datang <small>di Hoosk</small>";
+$lang['dash_unreachable']				= "Umpan Berita tidak dapat diakses";
+$lang['dash_recent']					= "Halaman yang baru saja diperbarui";
+$lang['dash_maintenance_message']                       = "Situs Anda saat ini dalam mode pemeliharaan, kunjungi halaman pengaturan untuk menonaktifkan ini dan memungkinkan pengunjung untuk melihat situs Anda.";
+$lang['dash_message']					= "Ini adalah dasbor Hoosk Anda, Dengan menggunakan bilah navigasi Anda dapat menambah atau mengubah pengguna, halaman atau menu navigasi di situs web Anda. Hoosk dibangun dengan menggunakan framework bootstrap, yang membuat situs web Anda sepenuhnya responsif.";
+$lang['feed_heading']					= "Berita Hoosk";
+
+
+//Settings Page
+$lang['settings_header']				= "Pengaturan";
+$lang['settings_message']				= "Beberapa pengaturan dasar untuk situs Anda ";
+$lang['settings_info']					= "Informasi";
+$lang['settings_name']					= "Nama Situs*";
+$lang['settings_footer']				= "Teks Footer Situs";
+$lang['settings_theme']					= "Tema yang digunakan:";
+$lang['settings_lang']					= "Bahasa dash yang sedang digunakan:";
+$lang['settings_logo']					= "Logo Situs";
+$lang['settings_favicon']				= "Favicon Situs";
+$lang['settings_maintenance']                           = "Aktifkan modus pemeliharaan?";
+$lang['settings_maintenance_heading']                   = "Heading Halaman Pemeliharaan:";
+$lang['settings_maintenance_meta']                      = "Deskripsi Meta Halaman Pemeliharaan";
+$lang['settings_maintenance_content']                   = "Isi Halaman Pemeliharaan";
+$lang['settings_additional_js']                         = "Javascript tambahan (seperti: Google Analytics) ini akan ditambahkan di akhir halaman, jangan lupa tambahkan tag script:";
+$lang['settings_image_error']                           = "Kesalahan! File ini bukan file gambar.";
+
+
+
+//Page - All Pages
+$lang['pages_header']                                   = "Semua Halaman";
+$lang['pages_table_page']                               = "Halaman";
+$lang['pages_table_updated']                            = "Diupdate";
+$lang['pages_table_created']                            = "Dibuat";
+$lang['pages_table_published']                          = "Dipublikasikan";
+$lang['pages_delete']                                   = "Hapus Halaman ";
+$lang['pages_delete_message']                           = "Ini permanen dan tidak bisa diurungkan…";
+$lang['pages_delete_home']                              = "Anda tidak dapat menghapus halaman beranda…";
+
+
+//Page - New Page/Edit Page
+$lang['pages_new_header']                               = "Halaman Baru";
+$lang['pages_edit_header']                              = "Ubah Halaman";
+$lang['pages_new_attributes']                           = "Atribut Halaman";
+$lang['pages_new_required']                             = "Semua bidang bertanda * wajib diisi!";
+$lang['pages_new_title']                                = "Judul Halaman / Meta *";
+$lang['pages_new_nav']                                  = "Judul Navigasi * (ini ditampilkan pada menu navigasi)";
+$lang['pages_new_keywords']                             = "Kata Kunci Meta";
+$lang['pages_new_description']                          = "Deskripsi meta";
+$lang['pages_new_url']                                  = "URL Halaman * (tidak boleh ada spasi atau karakter khusus)";
+$lang['pages_new_publish']                              = "Publikasikan Halaman?";
+$lang['pages_new_template']                             = "Templat Halaman";
+
+
+
+
+//Page - Jumbotron
+$lang['pages_jumbo_intro']                              = "a \"Jumbotron\" X adalah area heading besar di bagian atas halaman yang digunakan untuk pesan dengan teks besar, slider gambar atau keduanya!";
+$lang['pages_slider_header']                            = "Slider Gambar";
+$lang['pages_slider_btn']                               = "Opsi Slider Gambar:";
+$lang['pages_slider_enable']                            = "Aktifkan Slider?";
+$lang['pages_slider_add']                               = "Tambahkan Slide";
+$lang['pages_slider_title']                             = "Slide";
+$lang['pages_slider_alt']                               = "Tag Alt Gambar";
+$lang['pages_slider_link']                              = "Tautan Gambar";
+$lang['pages_jumbo_header']                             = "Isi Jumbotron";
+$lang['pages_jumbo_header_att']                         = "Jumbotron";
+$lang['pages_jumbo_enable']                             = "Aktifkan Jumbotron?";
+$lang['pages_jumbo_edit']                               = "Ubah Jumbotron";
+
+
+//Posts - All Posts
+$lang['posts_header']                                   = "Semua Postingan";
+$lang['posts_table_post']                               = "Judul Postingan";
+$lang['posts_table_category']                           = "Kategori";
+$lang['posts_table_posted']                             = "Tanggal Posting";
+$lang['posts_table_published']                          = "Dipublikasikan";
+$lang['posts_delete']                                   = "Hapus postingan ";
+$lang['posts_delete_message']                           = "Ini permanen dan tidak bisa diurungkan…";
+
+
+//Posts - New Post/Edit Post
+$lang['posts_new_header']                               = "Postingan Baru";
+$lang['posts_edit_header']                              = "Ubah Postingan";
+$lang['posts_new_attributes']                           = "Atribut Postingan";
+$lang['posts_new_required']                             = "Semua bidang bertanda * wajib diisi!";
+$lang['posts_new_title']                                = "Judul Posting / Meta *";
+$lang['posts_new_feature']                              = "Gambar Fitur";
+$lang['posts_new_excerpt']                              = "Kutipan*";
+$lang['posts_new_category']                             = "Kategori";
+$lang['posts_new_url']                                  = "URL Postingan * (tidak boleh ada spasi atau karakter khusus)";
+$lang['posts_new_date']                                 = "Tanggal";
+$lang['posts_new_published']                            = "Publikasikan?";
+
+//Posts - All Categories
+$lang['cat_header']                                     = "Semua Kategori";
+$lang['cat_table_category']                             = "Judul Kategori";
+$lang['cat_delete']                                     = "Hapus Kategori ";
+$lang['cat_delete_message']                             = "Ini permanen dan tidak dapat diurungkan ...";
+
+
+
+//Posts - New Category/Edit Category
+$lang['cat_new_header']                                 = "Kategori Baru";
+$lang['cat_edit_header']                                = "Ubah Kategori";
+$lang['cat_new_required']                               = "Semua bidang bertanda * wajib diisi!";
+$lang['cat_new_title']                                  = "Judul Kategori*";
+$lang['cat_new_url']                                    = "Slug URL Kategori* (tidak boleh mengandung karakter atau spasi khusus)";
+$lang['cat_new_description']                            = "Deskripsi Kategori";
+
+
+//Users - All Users
+$lang['user_header']                                    = "Seluruh Pengguna";
+$lang['user_username']                                  = "Username";
+$lang['user_email']                                     = "Alamat Email";
+$lang['user_delete']                                    = "Hapus Pengguna ";
+$lang['user_delete_message']                            = "Pastikan ini bukan satu-satunya akun pengguna Anda";
+
+
+//Users - New User/Edit User
+$lang['user_new_header']                                = "Akun Pengguna Baru";
+$lang['user_edit_header']                               = "Ubah Akun";
+$lang['user_new_username']                              = "Username";
+$lang['user_new_message']                               = "Username Anda untuk masuk dan tidak dapat diubah.";
+$lang['user_new_email']                                 = "Alamat Email";
+$lang['user_new_pass']                                  = "Password";
+$lang['user_new_confirm']                               = "Konfirmasi Password";
+
+
+//Navigation - All Menus
+$lang['menu_header']				= "Semua Menu";
+$lang['menu_table_title']			= "Judul Menu";
+$lang['menu_delete']				= "Hapus Nav ";
+$lang['menu_delete_message']                    = "Ini permanen dan tidak dapat diurungkan ...";
+$lang['menu_delete_message_header']             = "Anda tidak dapat menghapus menu header…";
+
+
+//Navigation - New Menu/Edit Menu
+$lang['menu_new_nav']				= "Navigasi";
+$lang['menu_new_pages']				= "Halaman";
+$lang['menu_new_nav_slug']			= "Slug Nav* (maks 10 karakter, tidak boleh ada spasi)";
+$lang['menu_new_nav_title']			= "Judul Nav*";
+$lang['menu_new_add_page']			= "Tambahkan Halaman";
+$lang['menu_new_select_page']                   = "Pilih Sebuah Halaman";
+$lang['menu_new_custom_title']                  = "Judul tautan khusus";
+$lang['menu_new_custom_link']                   = "URL tautan khusus (tambahkan http://)";
+$lang['menu_new_drop_down']			= "Tambahkan Bagian Drop Down";
+$lang['menu_new_drop_title']                    = "Judul";
+$lang['menu_new_drop_link']			= "Slug URL (ini ditambahkan ke URL, tidak ada spasi yang diizinkan)";
+$lang['menu_new_drop_error']                    = "Slug drop down Anda berisi spasi atau karakter khusus";
+
+
+//Social Page
+$lang['social_header']                          = "Pengaturan Sosial Media";
+$lang['social_message']                         = "Di sini Anda dapat mengatur tautan untuk berbagai akun sosial Anda. <br/> Tautan ini akan muncul di area yang ditentukan dari tema Anda.";
+$lang['social_enable']                          = "Untuk mengaktifkan ikon, centang kotak di sebelahnya.";
+$lang['social_twitter']                         = "Twitter";
+$lang['social_facebook']                        = "Facebook";
+$lang['social_google']                          = "Google+";
+$lang['social_pinterest']                       = "Pinterest";
+$lang['social_foursquare']                      = "Foursquare";
+$lang['social_linkedin']                        = "LinkedIn";
+$lang['social_myspace']                         = "Myspace";
+$lang['social_soundcloud']                      = "Soundcloud";
+$lang['social_spotify']                         = "Spotify";
+$lang['social_lastfm']                          = "LastFM";
+$lang['social_youtube']                         = "YouTube";
+$lang['social_vimeo']                           = "Vimeo";
+$lang['social_dailymotion']                     = "DailyMotion";
+$lang['social_vine']                            = "Vine";
+$lang['social_flickr']                          = "Flickr";
+$lang['social_instagram']                       = "Instagram";
+$lang['social_tumblr']                          = "Tumblr";
+$lang['social_reddit']                          = "Reddit";
+$lang['social_envato']                          = "Envato";
+$lang['social_github']                          = "Github";
+$lang['social_tripadvisor']                     = "TripAdvisor";
+$lang['social_stackoverflow']                   = "Stack Overflow";
+$lang['social_persona']                         = "Mozilla Persona";
+$lang['social_table_title']                     = "Situs Sosial Media";
+$lang['social_table_link']                      = "Tautan Profil";
+$lang['social_table_enabled']                   = "Aktifkan?";
+
+
+// COMMON BUTTONS
+$lang['btn_save']                               = "Simpan";
+$lang['btn_jumbotron']                          = "Jumbotron";
+$lang['btn_cancel']                             = "Batalkan";
+$lang['btn_delete']                             = "Hapus";
+$lang['btn_next']                               = "Selanjutnya";
+$lang['btn_back']                               = "Sebelumnya";
+$lang['btn_add']                                = "Tambah";
+
+
+$lang['option_yes']                             = "Ya";
+$lang['option_no']                              = "Tidak";
+
+
+
+
+
+// EMAILS
+$lang['email_reset_subject']                    = "Set Ulang Password Anda";
+$lang['email_reset_message']                    = "Silakan klik tautan di bawah ini untuk mengatur ulang kata sandi Anda.";
+
+
+// ERROR MESSAGES
+$lang['no_results']                             = "Tidak ada hasil yang ditemukan.";
+$lang['email_check']                            = "Alamat email ini tidak ada.";
+$lang['required']                               = "Bidang %s wajib diisi.";
+$lang['isset']                                  = "Bidang %s harus memiliki isian.";
+$lang['valid_email']                            = "Bidang %s harus berisi alamat email yang valid.";
+$lang['valid_emails']                           = "Bidang %s harus berisi semua alamat email yang valid.";
+$lang['valid_url']                              = "Bidang %s harus berisi URL yang valid.";
+$lang['valid_ip']                               = "Bidang %s harus berisi IP yang valid.";
+$lang['min_length']                             = "Panjang %s harus sepanjang setidaknya %s karakter.";
+$lang['max_length']                             = "Panjang %s tidak boleh lebih dari %s karakter.";
+$lang['exact_length']                           = "Panjang %s harus panjang persis %s karakter.";
+$lang['alpha']                                  = "Bidang %s hanya dapat berisi karakter alfabet.";
+$lang['alpha_numeric']                          = "Bidang %s hanya dapat berisi karakter alfa-numerik.";
+$lang['alpha_dash']                             = "Bidang %s hanya dapat berisi karakter alfa-numerik, garis bawah, dan garis putus-putus.";
+$lang['numeric']                                = "Bidang %s harus hanya berisi angka.";
+$lang['is_numeric']                             = "Bidang %s harus hanya berisi karakter numerik.";
+$lang['integer']                                = "Bidang %s harus berisi bilangan bulat.";
+$lang['regex_match']                            = "Bidang %s tidak dalam format yang benar.";
+$lang['matches']                                = "Bidang %s tidak cocok dengan bidang% s.";
+$lang['is_unique']                              = "%s ini sudah ada.";
+$lang['is_natural']                             = "Bidang %s hanya boleh berisi angka positif.";
+$lang['is_natural_no_zero']                     = "Bidang %s hanya boleh berisi angka positif.";
+$lang['decimal']                                = "Bidang %s harus berisi angka desimal.";
+$lang['less_than']                              = "Bidang %s harus berisi angka kurang dari %s.";
+$lang['greater_than']                           = "Bidang %s harus berisi angka kurang dari %s.";
