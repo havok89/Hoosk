e3e7dcea864453cb5c951e69d6997c8dd716c909
Callum Henry <callum.henry@rawcore101.com>
2020-05-01 14:52:22 +0100

    Fix for XSS

--- hoosk/hoosk0/config/config.php
+++ hoosk/hoosk0/config/config.php
@@ -411,7 +411,7 @@ $config['standardize_newlines'] = FALSE;
 |          for backwards compatibility purposes!
 |
 */
-$config['global_xss_filtering'] = FALSE;
+$config['global_xss_filtering'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
